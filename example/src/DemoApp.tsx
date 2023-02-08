830f085fe0b1375832054c126e153fa36df3aa7c
Tomas Psota <to.psota@gmail.com>
2023-02-08 11:19:16 +0100

    feat: add demo app

---
+++ example/src/DemoApp.tsx
@@ -0,0 +1,89 @@
+/* eslint-disable react-native/no-inline-styles */
+import * as React from 'react';
+import { Box, Flex, HStack, Text, VStack } from '@react-native-material/core';
+import CheckmarkCircle from '../assets/checkmark-circle-outline.png';
+import CloseCircle from '../assets/close-circle-outline.png';
+import TalsecLogo from '../assets/talsec-logo.png';
+import { Image } from 'react-native';
+import { Colors } from './styles';
+
+export const DemoApp = (props: any) => {
+  return (
+    <>
+      <Flex
+        fill
+        style={{
+          backgroundColor: Colors.background,
+          justifyContent: 'center',
+        }}
+      >
+        <VStack>
+          <Image source={TalsecLogo} style={{ alignSelf: 'center' }} />;
+          <Text
+            style={{
+              alignSelf: 'center',
+              fontWeight: 'bold',
+              fontSize: 25,
+              color: 'white',
+              paddingTop: 15,
+            }}
+          >
+            freeRASP checks:
+          </Text>
+          {props.checks.map((check: any, idx: number) => (
+            <Box
+              key={idx}
+              style={{
+                borderColor:
+                  check.status === 'ok'
+                    ? Colors.checkOkDark
+                    : Colors.checkNokDark,
+                backgroundColor:
+                  check.status === 'ok'
+                    ? Colors.checkOkLight
+                    : Colors.checkNokLight,
+                borderWidth: 1,
+                paddingHorizontal: 30,
+                paddingVertical: 4,
+                marginVertical: 8,
+                marginHorizontal: 20,
+                borderRadius: 15,
+              }}
+            >
+              <HStack style={{ justifyContent: 'space-between' }}>
+                <Text
+                  style={{
+                    color: check.status === 'ok' ? 'green' : 'rgb(200, 0, 0)',
+                    fontWeight: 'bold',
+                    alignSelf: 'center',
+                  }}
+                >
+                  {check.name}
+                </Text>
+                {check.status === 'ok' ? (
+                  <Image
+                    source={CheckmarkCircle}
+                    style={{
+                      tintColor: Colors.checkOkDark,
+                      width: 30,
+                      height: 30,
+                    }}
+                  />
+                ) : (
+                  <Image
+                    source={CloseCircle}
+                    style={{
+                      tintColor: Colors.checkNokDark,
+                      width: 30,
+                      height: 30,
+                    }}
+                  />
+                )}
+              </HStack>
+            </Box>
+          ))}
+        </VStack>
+      </Flex>
+    </>
+  );
+};
---
+++ example/src/checks.ts
@@ -0,0 +1,16 @@
+export const commonChecks = [
+  { name: 'Privileged Access', status: 'ok' },
+  { name: 'Debug', status: 'ok' },
+  { name: 'Simulator', status: 'ok' },
+  { name: 'App Integrity', status: 'ok' },
+  { name: 'Unofficial Store', status: 'ok' },
+  { name: 'Hooks', status: 'ok' },
+  { name: 'Device Binding', status: 'ok' },
+];
+
+export const iosChecks = [
+  { name: 'Device ID', status: 'ok' },
+  { name: 'Missing Secure Enclave', status: 'ok' },
+  { name: 'Passcode Change', status: 'ok' },
+  { name: 'Passcode', status: 'ok' },
+];

36e5057bbe70bfc7bab4fc0dfc1ec0bf82356035
Tomas Psota <to.psota@gmail.com>
2024-05-29 14:35:21 +0200

    feat(example): add vpn and dev mode detection

--- example/src/checks.ts
+++ example/src/checks.ts
@@ -1,15 +1,19 @@
 export const commonChecks = [
+  { name: 'App Integrity', status: 'ok' },
   { name: 'Privileged Access', status: 'ok' },
   { name: 'Debug', status: 'ok' },
-  { name: 'Simulator', status: 'ok' },
-  { name: 'App Integrity', status: 'ok' },
-  { name: 'Unofficial Store', status: 'ok' },
   { name: 'Hooks', status: 'ok' },
-  { name: 'Device Binding', status: 'ok' },
-  { name: 'Secure Hardware Not Available', status: 'ok' },
   { name: 'Passcode', status: 'ok' },
+  { name: 'Simulator', status: 'ok' },
+  { name: 'Secure Hardware Not Available', status: 'ok' },
+  { name: 'System VPN', status: 'ok' },
+  { name: 'Device Binding', status: 'ok' },
+  { name: 'Unofficial Store', status: 'ok' },
 ];
 
 export const iosChecks = [{ name: 'Device ID', status: 'ok' }];
 
-export const androidChecks = [{ name: 'Obfuscation Issues', status: 'ok' }];
+export const androidChecks = [
+  { name: 'Obfuscation Issues', status: 'ok' },
+  { name: 'Developer Mode', status: 'ok' },
+];

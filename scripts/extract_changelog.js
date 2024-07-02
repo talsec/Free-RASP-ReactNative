507cf1802c1a91e8b3894d6f00b51877b66a4c45
Tomas Psota <to.psota@gmail.com>
2024-07-02 08:40:31 +0200

    ci: update changelog parser

--- scripts/extract_changelog.js
+++ scripts/extract_changelog.js
@@ -5,8 +5,9 @@ const filePath = './CHANGELOG.md';
 const fileContent = fs.readFileSync(filePath, 'utf-8');
 
 const trimmedContent = fileContent
-  .replace(/\s*#\s*freeRASP.*/, '') // trim the latest version header
-  .replace(/\s*#\s*freeRASP([\s\S])*/, '') // trim everything after the latest changelog data
+  .replace(/([\s\S]*?)\s*(?=\s*##)/, '') // trim the # Changelog part
+  .replace(/\s*##\s*\[\d+\.\d+\.\d+\] - \d{4}-\d{2}-\d{2}.*/, '') // trim the latest version header
+  .replace(/\s*##\s*\[\d+\.\d+\.\d+\] - \d{4}-\d{2}-\d{2}([\s\S])*/, '') // trim everything after the latest changelog data
   .trim(); // trim leading and trailing whitespaces
 
 console.log(trimmedContent);
